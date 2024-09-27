a89c794acdb0089b3281d0f8d4c09a9e2d90d9d8
customersolutionscenterlinc <jpowell@lincsystems.com>
2024-09-27 11:15:02 -0400

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<?php
+$imagePath = 
+'path/to/LINClogo.jpg';
+?>
+<header>
+	<img src="<?php echo $imagePath; ?>" alt="LINC Logo" class = "logo">
+	<h1>LINC Logo</h1>
+	<p class="sub-title">
+		This web page shows the current inventory of Packaging Technical Service Demonstration Equipment.
+	</p>
+</header>
---
+++ includes/nav.php
@@ -0,0 +1,7 @@
+<nav>
+	<div class="brand">LINC Systems</div>
+	<div class="links">
+		<a data-active="index" href="index.php">Home</a>
+		<a data-active="contact" href="contact.php">Contact</a>
+	</div>
+</nav>
